6eb070eb752a77af499f7dd1df5810249943a9c7
vishaalram02 <vishaalram02@gmail.com>
2023-09-24 03:16:41 -0400

    remove svgr

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, "..", "");
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [react()],
     envDir: "..",
     envPrefix: "qstack",
     server: {
