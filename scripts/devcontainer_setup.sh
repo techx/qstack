d708768a3256fdb56e255636f1af61340696c374
Andrew Liu <andrewliu278@gmail.com>
2024-04-07 01:37:43 +0000

    dev container setup

---
+++ scripts/devcontainer_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Usage: bash scripts/devcontainer_setup.sh
+# Description: Installs dependencies and sets up the development environment
+# Automatically called when the devcontainer is created
+# Also, please keep this file idempotent, i.e., running multiple times is OK
+
+current_user=$(whoami)
+if [ "$current_user" != "vscode" ]; then
+    echo "Warning: Only run this script from within the dev container (unless you know what you're doing)!"
+    exit 1
+fi
+
+echo "Installing dependencies..."
+
+parallel --tag -j2 --line-buffer ::: "pip install --user -r requirements.txt" "cd client && rm -rf node_modules/ && npm install"
