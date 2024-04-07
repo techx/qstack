4e1627ac92210049aaf433246d8f08c23378ce6f
Andrew Liu <andrewliu278@gmail.com>
2024-04-07 01:54:43 +0000

    deploy script out of date

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-ssh -i ~/.ssh/techx-general.pem ubuntu@3.92.255.125 << EOF # replace ~/.ssh/techx-general.pem with the path to your SSH key
-    cd ~/qstack;
-    git pull;
-    cd client;
-    npm build;
-    sudo systemctl restart qstack;
-EOF
