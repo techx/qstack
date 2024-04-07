da3aa1c6b56a77d2b3ce26fe47d7e1a9133c5127
Andrew Liu <andrewliu278@gmail.com>
2024-04-07 01:37:43 +0000

    dev container setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ command, mode }) => {
       host: true,
       proxy: {
         "/api": {
-          target: "http://backend:3001",
+          target: "http://localhost:3001",
           changeOrigin: true,
         },
       },
